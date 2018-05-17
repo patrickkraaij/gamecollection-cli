7c6648b33e07f0ab50351d3446a5e47f423d8a4e
Patrick Kraaij <patrick.kraaij@gmail.com>
2018-05-17 14:57:37 +0200

    Refactored Promises to async/await

--- app.js
+++ app.js
@@ -1,188 +1,109 @@
 'use strict';
 
-const Table = require('cli-table'),
-	inquirer = require('inquirer'),
-	MongoClient = require('mongodb').MongoClient,
-	thegamesdb = require('thegamesdb'),
-	mongodb = require('./db'),
-	utils = require('./utils');
+const Table = require('cli-table');
+const thegamesdb = require('thegamesdb');
+const gcdb = require('gamecollection-mongodb');
+const utils = require('./utils');
 
 module.exports = {
-	getCollection: () => {
-		MongoClient.connect(mongodb.uri(), (err, db) => {
-			utils.connectedToDB(db);
+	getCollection: async () => {
+		const result = await gcdb.getGames();
 
-			db.collection('games').find().toArray((err, result) => {
-				if (result.length > 0) {
-					let table = new Table({
-						head: ['Title', 'Platform'],
-						colWidths: [60, 40]
-					});
+		let table = new Table({
+			head: ['Title', 'Platform'],
+			colWidths: [60, 40]
+		});
 
-					result.sort(utils.sortGameTitle).forEach((currentValue) => {
-						table.push([currentValue.title, currentValue.platform]);
-					});
+		result.forEach((currentValue) => {
+			table.push([currentValue.title, currentValue.platform]);
+		});
 
-					console.log(table.toString());
+		console.log(table.toString());
+		utils.calculateGames(result);
+	},
+	getCollectionPerPlatform: async () => {
+		const result = await gcdb.getGamesPerPlatform();
+		let table;
 
-					db.close();
-				}
-				utils.calculateGames(result);
+		result.forEach((currentValue) => {
+			table = new Table({
+				head: [currentValue.platform],
+				colWidths: [100]
 			});
+
+			currentValue.games.forEach((game) => {
+				table.push([game.title]);
+			});
+
+			table.push([`Total: ${currentValue.games.length} games`]);
+			console.log(table.toString());
 		});
 	},
-	getCollectionPerPlatform: () => {
-		MongoClient.connect(mongodb.uri(), (err, db) => {
-			utils.connectedToDB(db);
-
-			db.collection('games').distinct('platform', ((err, result) => {
-				if (result.length > 0) {
-					result.sort().forEach((platform) => {
-						db.collection('games').find({
-							platform: platform
-						}).toArray((err, games) => {
-							let gamesTable = new Table({
-								head: ['Title', 'Platform'],
-								colWidths: [60, 40]
-							});
-
-							games.sort(utils.sortGameTitle).forEach((item) => {
-								gamesTable.push([item.title, platform]);
-							});
-
-							gamesTable.push(['Total ' + platform + ' games', games.length]);
-
-							console.log(gamesTable.toString());
-						});
-					});
-					db.close();
-				}
-			}));
-		});
-	},
-	getGame: (game) => {
+	addGame: async (game) => {
 		if (game !== undefined) {
-			thegamesdb.getGamesList({
+			const thegamesdbResult = await thegamesdb.getGamesList({
 				name: utils.multipleWordsInput(process.argv)
-			}).then((data) => {
-				if (data.length > 0) {
-					let results = [];
-
-					data.forEach((currentValue) => {
-						results.push({
-							name: currentValue.title + ' ' + currentValue.platform,
-							value: currentValue.id
-						});
-					});
-
-					inquirer.prompt([{
-						type: 'checkbox',
-						message: 'Select a game to add:',
-						name: 'selectgame',
-						choices: results,
-						pageSize: 10,
-						validate: (answer) => {
-							if (answer.length < 1) {
-								return 'You must choose at least one game';
-							}
-							return true;
-						}
-					}]).then((answers) => {
-						answers.selectgame.some((currentValue) => {
-							thegamesdb.getGame({
-								id: currentValue
-							}).then((data) => {
-								inquirer.prompt([{
-									type: 'confirm',
-									message: 'Add selected game(s)?:',
-									name: 'insert'
-								}]).then((confirmAnswer) => {
-									if (confirmAnswer.insert === true) {
-										MongoClient.connect(mongodb.uri(), (err, db) => {
-											utils.connectedToDB(db);
-
-											db.collection('games').insertOne(mongodb.schema(data), (err) => {
-												utils.gameAdded(mongodb.schema(data).title);
-
-												db.close();
-											});
-										});
-									}
-								});
-							});
-						});
-					});
-				} else {
-					utils.noGamesFound();
-				}
 			});
-		} else {
+
+			if (thegamesdbResult.length > 0) {
+				const choices = thegamesdbResult.map(item => {
+					return {
+						name: `${item.title} | ${item.platform}`,
+						value: item.id
+					};
+				});
+
+				const addGamesDialog = await utils.dialog('checkbox', 'Select games to add:', 'gameIDs', choices, 10, 'You must choose at least one game');
+				const addGamesConfirmDialog = await utils.dialog('confirm', 'Add selected game(s)?:', 'isAdding');
+
+				if (addGamesConfirmDialog.isAdding) {
+					for (const gameID of addGamesDialog.gameIDs) {
+						let gameInformation = await thegamesdb.getGame({ id: gameID });
+						await gcdb.addGame(gameInformation);
+						utils.gameAdded(gameInformation.title);
+					}
+				}
+			}
+			else {
+				utils.noGamesFound();
+			}
+		}
+		else {
 			utils.pleaseEnterName();
 		}
 	},
-	deleteGame: (game) => {
+	removeGame: async (game) => {
 		if (game !== undefined) {
-			MongoClient.connect(mongodb.uri(), (err, db) => {
-				utils.connectedToDB(db);
+			const query = {
+				title: utils.multipleWordsInput(process.argv)
+			};
 
-				let query = {
-					title: utils.multipleWordsInput(process.argv)
-				};
+			const filteredGames = await gcdb.getGames(query);
 
-				db.collection('games').find(query).toArray((err, results) => {
-					let promptResults = [];
-
-					results.forEach((currentValue) => {
-						promptResults.push({
-							name: currentValue.title + ' ' + currentValue.platform,
-							value: currentValue._id
-						});
-					});
-
-					if (results.length > 0) {
-						inquirer.prompt([{
-							type: 'checkbox',
-							message: 'Remove a game:',
-							name: 'removegame',
-							choices: promptResults,
-							pageSize: 10,
-							validate: (answer) => {
-								if (answer.length < 1) {
-									return 'You must choose at least one game';
-								}
-								return true;
-							}
-						}]).then((answers) => {
-							inquirer.prompt([{
-								type: 'confirm',
-								message: 'Delete selected game(s)?:',
-								name: 'remove',
-							}]).then((confirmAnswer) => {
-								if (confirmAnswer.remove === true) {
-									MongoClient.connect(mongodb.uri(), (err, db) => {
-										utils.connectedToDB(db);
-
-										db.collection('games').deleteOne({
-											'_id': answers.removegame[0]
-										}, (err) => {
-											if (err) {
-												throw err;
-											}
-											utils.gameRemoved(results[0].title);
-											db.close();
-										});
-									});
-								}
-							});
-						});
-					} else {
-						utils.noGamesFound();
-					}
-
-					db.close();
+			if (filteredGames.length > 0) {
+				const choices = filteredGames.map(item => {
+					return {
+						name: `${item.title} | ${item.platform}`,
+						value: item._id
+					};
 				});
-			});
-		} else {
+
+				const removeGamesDialog = await utils.dialog('checkbox', 'Remove a game:', 'removeGame', choices, 10, 'You must choose at least one game');
+				const removeGamesConfirmDialog = await utils.dialog('confirm', 'Remove selected game(s)?:', 'isRemoving');
+
+				if (removeGamesConfirmDialog.isRemoving) {
+					for (const gameID of removeGamesDialog.removeGame) {
+						let gameToBeRemoved = await gcdb.getGame(gameID);
+						await gcdb.deleteGame(gameID);
+						utils.gameRemoved(gameToBeRemoved.title);
+					}
+				}
+			}
+			else {
+				utils.noGamesFound();
+			}
+		}
+		else {
 			utils.pleaseEnterName();
 		}
 	}
